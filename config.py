19d230bd6218f8ea3cccef1619a58fa5b26d3661
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-08-06 14:38:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,12 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", 12345))
-    BOT_SESSION = os.environ.get("BOT_SESSION", "bot")
+    API_ID = int(os.environ.get("API_ID", 12345)
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     CAPTION = os.environ.get("CAPTION", "")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "document")
     OWNER_ID = os.environ.get("OWNER_ID", 12345)
-    SESSION = os.environ.get("SESSION", "Media_search")
+    BOT_SESSION = os.environ.get("SESSION", "Media_search")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
