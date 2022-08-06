092fa36d632b3e2ea603fbb906050333b51f3fef
sahid malik <103699299+malik7983@users.noreply.github.com>
2022-08-06 08:41:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,9 @@ class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     CAPTION = os.environ.get("CAPTION", "")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "document")
     OWNER_ID = os.environ.get("OWNER_ID", 12345)
-    SESSION = os.environ.get("SESSION")
+    SESSION = os.environ.get("SESSION", "Media_search")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
